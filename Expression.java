1dc595357da5741fee4a22697d641514f5ae71c8
Samantha Phillips <samanthaphillips886@gmail.com>
2026-02-07 13:43:31 -0600

    First updates to expression for Q1

--- Expression.java
+++ Expression.java
@@ -5,50 +5,55 @@ import java.util.ArrayList;
  *   @author Dave Reed
  *   @version 1/20/26
  */
+
+//  The Boolean operator '!' (not) is currently implemented, but the '&' (and) and '|' (or) operators are not.
+// Modify the evaluate method of the Expression class so that these two operators are defined for Boolean values.
+// Note: if these operators are applied to non-Boolean values, your code should throw an exception with a meaningful error message.
+
 public class Expression {
-    private Token tok;					    // used for simple expressions (no operators)
-    private Token op;                       // used for operator
-    private ArrayList<Expression> exprs;	// operator expressions or list contents
+
+    private Token tok; // used for simple expressions (no operators)
+    private Token op; // used for operator
+    private ArrayList<Expression> exprs; // operator expressions or list contents
 
     /**
      * Creates an expression from the specified TokenStream.
      *   @param input the TokenStream from which the program is read
      */
     public Expression(TokenStream input) throws Exception {
-    	this.tok = input.next();
-    	if (this.tok.toString().equals("(")) {
-			this.exprs = new ArrayList<Expression>();
-    		if (input.lookAhead().getType() == Token.Type.UNARY_OP) {
-    			this.op = input.next();
-    			this.exprs.add(new Expression(input));
-    		} 
-    		else {
-    			this.exprs.add(new Expression(input));
-    			if (input.lookAhead().getType() == Token.Type.BINARY_OP) {
-    			    this.op = input.next();
-    			    this.exprs.add(new Expression(input));
-    			}
-    			else {
-    				throw new Exception("SYNTAX ERROR: Malformed expression");
-    			}
+        this.tok = input.next();
+        if (this.tok.toString().equals("(")) {
+            this.exprs = new ArrayList<Expression>();
+            if (input.lookAhead().getType() == Token.Type.UNARY_OP) {
+                this.op = input.next();
+                this.exprs.add(new Expression(input));
+            } else {
+                this.exprs.add(new Expression(input));
+                System.out.println("BINARY operator found");
+                if (input.lookAhead().getType() == Token.Type.BINARY_OP) {
+                    this.op = input.next();
+                    this.exprs.add(new Expression(input));
+                } else {
+                    throw new Exception("SYNTAX ERROR: Malformed expression");
+                }
             }
-    		if (!(input.next().toString().equals(")"))) {
-    		    throw new Exception("SYNTAX ERROR: Malformed expression");
-    		}
+            if (!(input.next().toString().equals(")"))) {
+                throw new Exception("SYNTAX ERROR: Malformed expression");
+            }
+        } else if (this.tok.toString().equals("[")) {
+            this.exprs = new ArrayList<Expression>();
+            while (!input.lookAhead().toString().equals("]")) {
+                this.exprs.add(new Expression(input));
+            }
+            input.next();
+        } else if (
+            this.tok.getType() != Token.Type.IDENTIFIER &&
+            this.tok.getType() != Token.Type.INT_LITERAL &&
+            this.tok.getType() != Token.Type.STR_LITERAL &&
+            this.tok.getType() != Token.Type.BOO_LITERAL
+        ) {
+            throw new Exception("SYNTAX ERROR: malformed expression");
         }
-    	else if (this.tok.toString().equals("[")) {
-			this.exprs = new ArrayList<Expression>(); 
-			while (!input.lookAhead().toString().equals("]")) {
-				this.exprs.add(new Expression(input));
-			}
-			input.next();
-    	}
-    	else if (this.tok.getType() != Token.Type.IDENTIFIER &&
-                 this.tok.getType() != Token.Type.INT_LITERAL &&    
-                 this.tok.getType() != Token.Type.STR_LITERAL &&
-                 this.tok.getType() != Token.Type.BOO_LITERAL) {
-          throw new Exception("SYNTAX ERROR: malformed expression");
-    	}
     }
 
     /**
@@ -56,40 +61,45 @@ public class Expression {
      *   @return the value represented by the expression
      */
     public DataValue evaluate() throws Exception {
-    	if (this.op == null) {
-    		if (this.tok.toString().equals("[")) {
-            	ArrayList<DataValue> vals = new ArrayList<DataValue>();
-            	for (Expression e : this.exprs) {
-            		vals.add(e.evaluate());
-            	}
-            	return new ListValue(vals);
-            }
-    		else if (this.tok.getType() == Token.Type.IDENTIFIER) {
+        if (this.op == null) {
+            if (this.tok.toString().equals("[")) {
+                ArrayList<DataValue> vals = new ArrayList<DataValue>();
+                for (Expression e : this.exprs) {
+                    vals.add(e.evaluate());
+                }
+                return new ListValue(vals);
+            } else if (this.tok.getType() == Token.Type.IDENTIFIER) {
                 if (!Interpreter.MEMORY.isDeclared(this.tok)) {
-                    throw new Exception("RUNTIME ERROR: variable " + this.tok + " is undeclared");
+                    throw new Exception(
+                        "RUNTIME ERROR: variable " + this.tok + " is undeclared"
+                    );
                 }
                 return Interpreter.MEMORY.lookupValue(this.tok);
             } else if (this.tok.getType() == Token.Type.INT_LITERAL) {
                 return new IntegerValue(Integer.parseInt(this.tok.toString()));
             } else if (this.tok.getType() == Token.Type.STR_LITERAL) {
-            	String s = this.tok.toString();
-                return new StringValue(s.substring(1, s.length()-1));
+                String s = this.tok.toString();
+                return new StringValue(s.substring(1, s.length() - 1));
             } else if (this.tok.getType() == Token.Type.BOO_LITERAL) {
                 return new BooleanValue(Boolean.valueOf(this.tok.toString()));
             }
         } else if (this.op.getType() == Token.Type.UNARY_OP) {
             DataValue rhs = this.exprs.get(0).evaluate();
-            
             if (this.op.toString().equals("!")) {
+                System.out.println("UHERE " + this.op);
                 if (rhs.getType() == DataValue.Type.BOOLEAN) {
                     boolean b2 = ((Boolean) (rhs.getValue()));
                     return new BooleanValue(!b2);
                 }
             }
-            throw new Exception("RUNTIME ERROR: Type mismatch in unary expression");
+
+            throw new Exception(
+                "RUNTIME ERROR: Type mismatch in unary expression"
+            );
         } else if (this.op.getType() == Token.Type.BINARY_OP) {
             DataValue lhs = this.exprs.get(0).evaluate();
             DataValue rhs = this.exprs.get(1).evaluate();
+            ArrayList<Boolean> boolCheck = new ArrayList<>();
 
             if (lhs.getType() == rhs.getType()) {
                 if (op.toString().equals("=")) {
@@ -100,31 +110,63 @@ public class Expression {
                     return new BooleanValue(lhs.compareTo(rhs) > 0);
                 } else if (op.toString().equals("<")) {
                     return new BooleanValue(lhs.compareTo(rhs) < 0);
-                } else if (lhs.getType() == DataValue.Type.STRING) {
-                    String str1 = (String)lhs.getValue();
-                    String str2 = (String)rhs.getValue();
-                    
-                    if (op.toString().equals("+")) {
-                        return new StringValue(str1 + str2);
+                } else if (this.op.toString().equals("&")) {
+                    if (rhs.getType() == DataValue.Type.BOOLEAN) {
+                        boolean b2 = ((Boolean) (rhs.getValue()));
+                        if (b2 == true) {
+                            boolCheck.add(true);
+                        } else {
+                            boolCheck.add(false);
+                        }
                     }
-                } else if (lhs.getType() == DataValue.Type.INTEGER) {
-                    int num1 = ((Integer) (lhs.getValue()));
-                    int num2 = ((Integer) (rhs.getValue()));
+                    System.out.println("boolCheck: " + boolCheck);
+                    if (boolCheck.contains(false)) {
+                        return new BooleanValue(false);
+                    } else {
+                        return new BooleanValue(true);
+                    }
+                } else if (this.op.toString().equals("|")) {
+                    if (rhs.getType() == DataValue.Type.BOOLEAN) {
+                        boolean b2 = ((Boolean) (rhs.getValue()));
+                        if (b2 == true) {
+                            boolCheck.add(true);
+                        } else {
+                            boolCheck.add(false);
+                        }
+                        System.out.println("boolCheck: " + boolCheck);
+                        if (boolCheck.contains(true)) {
+                            return new BooleanValue(true);
+                        } else {
+                            return new BooleanValue(false);
+                        }
+                    } else if (lhs.getType() == DataValue.Type.STRING) {
+                        String str1 = (String) lhs.getValue();
+                        String str2 = (String) rhs.getValue();
 
-                    if (op.toString().equals("+")) {
-                        return new IntegerValue(num1 + num2);
-                    } else if (op.toString().equals("*")) {
-                        return new IntegerValue(num1 * num2);
-                    } else if (op.toString().equals("/")) {
-                        return new IntegerValue(num1 / num2);
-                    } else if (op.toString().equals("%")) {
-                        return new IntegerValue(num1 % num2);
-                    } else if (op.toString().equals("^")) {
-                        return new IntegerValue((int)Math.pow(num1, num2));
+                        if (op.toString().equals("+")) {
+                            return new StringValue(str1 + str2);
+                        }
+                    } else if (lhs.getType() == DataValue.Type.INTEGER) {
+                        int num1 = ((Integer) (lhs.getValue()));
+                        int num2 = ((Integer) (rhs.getValue()));
+
+                        if (op.toString().equals("+")) {
+                            return new IntegerValue(num1 + num2);
+                        } else if (op.toString().equals("*")) {
+                            return new IntegerValue(num1 * num2);
+                        } else if (op.toString().equals("/")) {
+                            return new IntegerValue(num1 / num2);
+                        } else if (op.toString().equals("%")) {
+                            return new IntegerValue(num1 % num2);
+                        } else if (op.toString().equals("^")) {
+                            return new IntegerValue((int) Math.pow(num1, num2));
+                        }
                     }
-                } 
+                }
+                throw new Exception(
+                    "RUNTIME ERROR: Type mismatch in binary expression"
+                );
             }
-            throw new Exception("RUNTIME ERROR: Type mismatch in binary expression");
         }
         return null;
     }
@@ -135,22 +177,27 @@ public class Expression {
      */
     public String toString() {
         if (this.op == null) {
-        	if (this.tok.toString().equals("[")) {
-            	String message = "[";
-            	for (Expression e: this.exprs) {
-            		message += e + " ";
-            	}
-            	return message.trim() + "]";
+            if (this.tok.toString().equals("[")) {
+                String message = "[";
+                for (Expression e : this.exprs) {
+                    message += e + " ";
+                }
+                return message.trim() + "]";
+            } else {
+                return this.tok.toString();
             }
-        	else {
-        		return this.tok.toString();
-        	}
-        }
-        else if (this.op.getType() == Token.Type.UNARY_OP){
-	        return "(" + this.op + " " + this.exprs.get(0) + ")";
-        }
-        else {
-	        return "(" + this.exprs.get(0) + " " + this.op + " " + this.exprs.get(1) + ")";
+        } else if (this.op.getType() == Token.Type.UNARY_OP) {
+            return "(" + this.op + " " + this.exprs.get(0) + ")";
+        } else {
+            return (
+                "(" +
+                this.exprs.get(0) +
+                " " +
+                this.op +
+                " " +
+                this.exprs.get(1) +
+                ")"
+            );
         }
     }
 }
